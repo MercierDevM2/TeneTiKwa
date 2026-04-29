56d12c3387c730aed499829f3d8cd704ee607c3d
MercierDev <167873187+MercierDevM2@users.noreply.github.com>
2026-04-29 16:28:10 +0100

    Update client.js

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,13 +1,15 @@
 // utils/supabase/client.js
-import { createClient } from "@supabase/supabase-js";  
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!url || !key) {
-    throw new Error("Les variables d'environnement Supabase sont manquantes");
+    throw new Error(
+      "Les variables d'environnement Supabase sont manquantes"
+    );
   }
 
-  return createClient(url, key); 
+  return createBrowserClient(url, key);
 }
